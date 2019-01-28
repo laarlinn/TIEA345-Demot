baa464c9910abf881897cf5834de009d06f4b2fc
Lauri Linna <33934448+laarlinn@users.noreply.github.com>
2019-01-28 10:32:06 +0200

    Update lampotila_gdocs.py

--- demo3/lampotila_gdocs.py
+++ demo3/lampotila_gdocs.py
@@ -10,11 +10,13 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name('google_creds.jso
 gc = gspread.authorize(credentials)
 sh = gc.open("tiea345-temps")
 worksheet = sh.sheet1
-
-humidity, temp = lampotila.get_temp()
-print(humidity, temp)
-
 # Käytin tätä aluksi taulukon tyhjentämiseen.
 # worksheet.resize(1)
 
-worksheet.append_row([str(temp), str(humidity), str(datetime.datetime.now())])
+while True:
+    try:
+        humidity, temp = lampotila.get_temp()
+        print(humidity, temp)
+        worksheet.append_row([str(temp), str(humidity), str(datetime.datetime.now())])
+    except KeyboardInterrupt:
+        print("Stopping")
